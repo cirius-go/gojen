9f48b5a54b3a5d9b34b74370d7cbaa8286dfce20
Minh Hieu Tran <hieu@buuuk.com>
2024-07-22 11:41:03 +0700

    update(funcs): added more func to handle specific case in golang

--- config.go
+++ config.go
@@ -1,8 +1,13 @@
 package gojen
 
+import (
+	"text/template"
+)
+
 // Config is a struct that holds the configuration for the Gojen instance.
 type Config struct {
-	dryRun bool
+	dryRun         bool
+	customPipeline map[string]any
 }
 
 // SetDryRun sets the dryRun field of the Config struct.
@@ -11,9 +16,26 @@ func (c *Config) SetDryRun(dryRun bool) *Config {
 	return c
 }
 
+// RegisterPipeline registers a custom pipeline.
+func (c *Config) RegisterPipeline(name string, pipeline any) *Config {
+	switch v := pipeline.(type) {
+	case func(template.FuncMap, string) string:
+		// The custom pipeline is a function that takes a FuncMap and a string as
+		// arguments and returns a string.
+		c.customPipeline[name] = func(s string) string {
+			return v(templateFuncs, s)
+		}
+	default:
+		c.customPipeline[name] = pipeline
+	}
+	c.customPipeline[name] = pipeline
+	return c
+}
+
 // C returns a new Config struct.
 func C() *Config {
 	return &Config{
-		dryRun: false,
+		dryRun:         false,
+		customPipeline: make(map[string]any),
 	}
 }
