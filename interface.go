b82fc04d13614e7ef0bf0d29d5e00eb39e393e43
Minh Hieu Tran <hieu@buuuk.com>
2024-08-19 09:41:34 +0700

    v1.1.3-alpha

--- interface.go
+++ interface.go
@@ -3,6 +3,7 @@ package gojen
 // Sequence is an interface that holds the sequence of the template.
 type Sequence interface {
 	S(n string, is ...int) *sequence
+	M(n string, is ...int) *sequence
 	ForwardCtx(filteredNames ...string) *sequence
 	When(i int, next func(sub Sequence)) *sequence
 }
